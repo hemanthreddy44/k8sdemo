ccfee504e990b362595e04b06d8ae2d41e6dae5f
hemanthreddy44 <65831040+hemanthreddy44@users.noreply.github.com>
2025-03-25 03:37:38 +0530

    Update master.sh

--- installation/master.sh
+++ installation/master.sh
@@ -1,1 +1,67 @@
+#!/bin/bash
+set -e
+
+# Set hostname
+echo "-------------Setting hostname-------------"
+hostnamectl set-hostname master
+# Disable swap
+echo "-------------Disabling swap-------------"
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+
+
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+ 
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+sudo systemctl restart kubelet
+ 
+echo "--------Installing Containerd with its prerequisite"
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+ 
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+sudo apt-get update && sudo apt-get install -y containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/  SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+echo "-------------Pulling Kubeadm Images -------------"
+kubeadm config images pull
+ 
+echo "-------------Running kubeadm init-------------"
+kubeadm init
+
+echo "----------------- Installing Helm-------------"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+echo "-------------Deploying Cilium-------------"
+helm repo add cilium https://helm.cilium.io/
+helm install cilium cilium/cilium --version 1.17.2 --namespace kube-system
+
+echo "------------Remove taints and tolerations----"
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+echo "-------------Creating file with join command-------------"
+echo `kubeadm token create --print-join-command` > ./join-command.cat
 
